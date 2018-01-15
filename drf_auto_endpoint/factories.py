78501ef301ca004028bd21c8f82a16cc61b1fa7f
Roger Agustin <roger@MacBook-MacBook-Pro-de-Roger.local>
2018-01-15 17:07:18 +0100

    Print some console logs to understand options available in the model_field.

--- drf_auto_endpoint/factories.py
+++ drf_auto_endpoint/factories.py
@@ -93,17 +93,13 @@ def serializer_factory(endpoint=None, fields=None, base_class=None, model=None):
     """
     for field in meta_attrs['fields']:
         try:
-            #print(dir(meta_attrs['model']))
-            #if meta_attrs['model']=='ProductCategory':
-            #    print(dir(model_field))
-
             model_field = endpoint.model._meta.get_field(field)
             if "children" in dir(meta_attrs['model']):
-                cls_attrs['children'] = RecursiveSerializer(many=True, read_only=True)
-            #elif str(model_field.get_internal_type()) == "ForeignKey":
-            #    cls_attrs[model_field.name] = serializers.StringRelatedField(many=False)
-            #elif str(model_field.get_internal_type()) == "ManyToManyField":
-            #    cls_attrs[model_field.name] = serializers.StringRelatedField(many=True)
+                cls_attrs['children'] = eval(RecursiveSerializer(many=True, read_only=True))
+            elif str(model_field.get_internal_type()) == "ForeignKey":
+                cls_attrs[model_field.name] = serializers.StringRelatedField(many=False)
+            elif str(model_field.get_internal_type()) == "ManyToManyField":
+                cls_attrs[model_field.name] = serializers.StringRelatedField(many=True)
 
         except FieldDoesNotExist:
             pass

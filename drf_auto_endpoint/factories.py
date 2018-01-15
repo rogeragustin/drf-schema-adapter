f92467aa993f5d52dd4f96e4485fc65b7cc0d11f
Roger Agustin <roger@MacBook-MacBook-Pro-de-Roger.local>
2018-01-15 17:01:21 +0100

    Print some console logs to understand options available in the model_field.

--- drf_auto_endpoint/factories.py
+++ drf_auto_endpoint/factories.py
@@ -98,14 +98,12 @@ def serializer_factory(endpoint=None, fields=None, base_class=None, model=None):
             #    print(dir(model_field))
 
             model_field = endpoint.model._meta.get_field(field)
-            print("children" in dir(meta_attrs['model']))
             if "children" in dir(meta_attrs['model']):
-                print("hola")
                 cls_attrs['children'] = RecursiveSerializer(many=True, read_only=True)
-            elif str(model_field.get_internal_type()) == "ForeignKey":
-                cls_attrs[model_field.name] = serializers.StringRelatedField(many=False)
-            elif str(model_field.get_internal_type()) == "ManyToManyField":
-                cls_attrs[model_field.name] = serializers.StringRelatedField(many=True)
+            #elif str(model_field.get_internal_type()) == "ForeignKey":
+            #    cls_attrs[model_field.name] = serializers.StringRelatedField(many=False)
+            #elif str(model_field.get_internal_type()) == "ManyToManyField":
+            #    cls_attrs[model_field.name] = serializers.StringRelatedField(many=True)
 
         except FieldDoesNotExist:
             pass

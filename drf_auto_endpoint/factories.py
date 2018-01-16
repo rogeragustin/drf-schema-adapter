b81b94a500306214e0a16c05546ecc1deab6e0e6
Roger Agustin <roger@MacBook-MacBook-Pro-de-Roger.local>
2018-01-16 20:14:07 +0100

    Ajustos per retornar sempre nested serializers

--- drf_auto_endpoint/factories.py
+++ drf_auto_endpoint/factories.py
@@ -139,7 +139,8 @@ def serializer_factory(endpoint=None, fields=None, base_class=None, model=None):
     else:
         return type(cls_name, (NullToDefaultMixin, WritableNestedModelSerializer, ), cls_attrs)
     """
-    print(type(cls_name, (NullToDefaultMixin, base_class,), cls_attrs))
+    print(cls_name)
+    print(cls_attrs)
     return type(cls_name, (NullToDefaultMixin, base_class,), cls_attrs)
 
 

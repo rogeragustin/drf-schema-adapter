0b93e601d7df56ad790423797f8b4f7d7d8ae410
Roger Agustin <roger@MacBook-MacBook-Pro-de-Roger.local>
2018-01-16 21:32:45 +0100

    Ajustos per retornar sempre nested serializers

--- drf_auto_endpoint/factories.py
+++ drf_auto_endpoint/factories.py
@@ -116,10 +116,11 @@ def serializer_factory(endpoint=None, fields=None, base_class=None, model=None):
                 # cls_attrs[model_field.name] = serializers.StringRelatedField(many=False)
                 # cls_attrs[model_field.name] = RecursiveField(required=False, allow_null=True, many=False)
                 print(model_field.name)
-                import serializer_factory(model=model_field.related_model)
-                nested_serializer = '{}Serializer()'.format(model_field.related_model.__name__)
+                nested_serializer = serializer_factory(model=model_field.related_model)
+                import nested_serializer
+                nest_ser = '{}Serializer()'.format(model_field.related_model.__name__)
                 #print(nested_serializer)
-                cls_attrs[model_field.name] = eval(nested_serializer)
+                cls_attrs[model_field.name] = eval(nest_ser)
             """
             elif str(model_field.get_internal_type()) == "ManyToManyField":
                 #cls_attrs[model_field.name] = serializers.StringRelatedField(many=True)

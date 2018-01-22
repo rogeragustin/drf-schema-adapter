cf745ef96b070414edaddfe140ffaecaa3c15196
Roger Agustin <roger@MacBook-MacBook-Pro-de-Roger.local>
2018-01-22 21:39:37 +0100

    Aven

--- drf_auto_endpoint/factories.py
+++ drf_auto_endpoint/factories.py
@@ -208,7 +208,7 @@ def serializer_factory(endpoint=None, fields=None, base_class=None, model=None):
 
         try:
             through_model_name = M2MRelations(field, 'through_model')
-            from endpoint.model._meta.app_label import eval(through_model_name)
+            exec("from endpoint.model._meta.app_label import through_model_name")
             through_model = eval(through_model_name)
             print(through_model)
             print(field.field.remote_field.name)

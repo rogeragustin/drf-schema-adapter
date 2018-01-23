12b928a6a680c2e2b517f4a367ea94a5b4b2e97f
Roger Agustin <roger@MacBook-MacBook-Pro-de-Roger.local>
2018-01-23 09:38:30 +0100

    Aven

--- drf_auto_endpoint/factories.py
+++ drf_auto_endpoint/factories.py
@@ -93,6 +93,101 @@ def M2MRelations(field, attr):
         return field.through.__name__+"Serializer"
 
 
+def create(self, validated_data):
+    model = self.Meta.model
+
+    # 1. Take out from the validated_data all the many to many fields information (it should be stored into the
+    # intermediate model instance) and store it temporarily in auxiliary variables.
+    #for field in [rel]
+    for f in [f for f in model._meta.get_fields() if f.many_to_many and not f.auto_created]:
+        field = eval("model.{}".format(f.name))
+
+        # Assign relfield_data = None
+        exec ("{0}{1} = {2}".format(f.name, "_data", None))  # E.g: ingredients_data = None
+
+        if validated_data.get(M2MRelations(field, 'related_name'), None) is not None:
+            exec (
+            "{0}{1} = validated_data.pop('{2}')".format(f.name, "_data", M2MRelations(field, 'related_name')))
+            # E.g:
+            # if validated_data.get("productingredient_set", None) is not None:
+            #    ingredients_data = validated_data.pop('productingredient_set')
+
+    # 2. Create an instance of the model with the base validated_data
+    model_instance = model.objects.create(**validated_data)
+    # E.g: product = Product.objects.create(**validated_data)
+
+    # 3. For each auxiliary variable, fill the corresponding intermediate model with this information.
+    for f in [f for f in model._meta.get_fields() if f.many_to_many and not f.auto_created]:
+        field = eval("model.{}".format(f.name))
+
+        if vars()[f.name + "_data"] is not None:
+            for rel_model_instance in vars()[f.name + "_data"]:
+                exec (
+                "{0}.objects.create({1}={2}, **rel_model_instance)".format(M2MRelations(field, 'through_model'),
+                                                                           M2MRelations(field, 'related_field'),
+                                                                           'model_instance'))
+                # E.g:
+                # if ingredients_data is not None:
+                #    for ingredient in ingredients_data:
+                #        ProductIngredient.objects.create(product=product, **ingredient)
+                #        # la comanda **ingredient el que fa es, donat un diccionari, treure-li els corxets i
+                #        # transformar els : en =.
+
+    return model_instance
+
+
+def update(self, instance, validated_data):
+    model = self.Meta.model
+
+    # Pop data from validated.
+    for f in [f for f in model._meta.get_fields() if f.many_to_many and not f.auto_created]:
+        field = eval("model.{}".format(f.name))
+
+        # Assign relfield_data = None
+        exec ("{0}{1} = {2}".format(f.name, "_data", None))  # E.g: ingredients_data = None
+
+        # relfield_data = validated_data.pop(relfield)
+        if validated_data.get(M2MRelations(field, 'related_name'), None) is not None:
+            exec (
+                "{0}{1} = validated_data.pop('{2}')".format(f.name, "_data", M2MRelations(field, 'related_name')))
+
+    for item in validated_data:
+        if model._meta.get_field(item):
+            setattr(instance, item, validated_data[item])
+
+    # Load previous data and either update it with the new one. In case no data existed, create a new record.
+    for f in [f for f in model._meta.get_fields() if f.many_to_many and not f.auto_created]:
+        field = eval("model.{}".format(f.name))
+
+        # Load previous subinstance
+        submodel_instance = eval("{0}.objects.filter({1}=instance)".format(M2MRelations(field, 'through_model'),
+                                                                           M2MRelations(field, 'related_field')))
+
+        # Set new content for intermediary model
+        if eval(f.name + "_data") is not None:
+            for rel_model_instance in eval(f.name + "_data"):
+                if submodel_instance.exists():
+                    print (
+                        "{0}.objects.filter({3}=instance).update({1}={2},updated_at=datetime.now(), **rel_model_instance)".
+                            format(M2MRelations(field, 'through_model'),
+                                   M2MRelations(field, 'related_field'),
+                                   'instance',
+                                   M2MRelations(field, 'related_field')))
+                    exec (
+                        "{0}.objects.filter({3}=instance).update({1}={2},updated_at=datetime.now(), **rel_model_instance)".
+                        format(M2MRelations(field, 'through_model'),
+                               M2MRelations(field, 'related_field'),
+                               'instance',
+                               M2MRelations(field, 'related_field')))
+                else:
+                    exec("{0}.objects.create({1}={2}, **rel_model_instance)".format(
+                         M2MRelations(field, 'through_model'),
+                         M2MRelations(field, 'related_field'),
+                         'instance'))
+
+    instance.save()
+    return instance
+
 
 def related_serializer_factory(endpoint=None, fields=None, base_class=None, model=None):
     if model is not None:
@@ -130,7 +225,6 @@ def related_serializer_factory(endpoint=None, fields=None, base_class=None, mode
                     cls_attrs[meta_field] = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
             except FieldDoesNotExist:
                 cls_attrs[meta_field] = serializers.ReadOnlyField()
-    print(type(cls_name, (NullToDefaultMixin, base_class,), cls_attrs))
 
     return type(cls_name, (NullToDefaultMixin, base_class,), cls_attrs)
 
@@ -168,24 +262,6 @@ def serializer_factory(endpoint=None, fields=None, base_class=None, model=None):
     ######
     # BEGINNING - ADDED CODE
     ######
-    """
-    When constructing a serializer, which envolves a fk, return the __str__(self) set in the model,
-    instead of the id. If we detect a children model_field, we will return a tree-shaped serializer.
-    """
-    """
-    for field in meta_attrs['fields']:
-        try:
-            model_field = endpoint.model._meta.get_field(field)
-            if model_field.name == 'children':
-                cls_attrs[model_field.name] = RecursiveField(required=False, allow_null=True, many=True)
-
-            elif str(model_field.get_internal_type()) == "ForeignKey":
-                cls_attrs[model_field.name] = serializers.StringRelatedField(many=False)
-            elif str(model_field.get_internal_type()) == "ManyToManyField":
-                cls_attrs[model_field.name] = serializers.StringRelatedField(many=True)
-        except FieldDoesNotExist:
-            pass
-    """
     # Special treatment for particular cases of foreignkey fields.
     # Send neseted recursive field with all the info in the case of a "children" file (typical for categories)
     for f in [f for f in list(filter(lambda x: x!= '__str__', meta_attrs['fields'])) if endpoint.model._meta.get_field(f).name == 'children']:
@@ -212,31 +288,13 @@ def serializer_factory(endpoint=None, fields=None, base_class=None, model=None):
                    and f.name != M2MRelations(field,'related_field')
             ]
 
-            print(through_fields)
-
             SubSerializer = related_serializer_factory(model=through_model, fields = through_fields)
-            print("hola")
-            print(SubSerializer)
 
-            print (
-                "cls_attrs[field.field.name] = {0}(source='{1}', "
-                "many=True, required=False, allow_null=True)".format(SubSerializer.__name__, M2MRelations(field, 'related_name'))
-            )
-
-            #cls_attrs[field.field.name] = SubSerializer("source='{0}', many=True, required=False, allow_null=True".format(
-            #    M2MRelations(field, 'related_name')))
             cls_attrs[field.field.name] = SubSerializer(source=M2MRelations(field, 'related_name'), many=True, required=False, allow_null=True)
 
-            print("Pollo")
-            print(cls_attrs[field.field.name])
-
-            #cls_attrs[field.field.name] = eval("{0}(source='{1}', "
-            #                                   "many=True, required=False, allow_null=True)".format(
-            #    SubSerializer, M2MRelations(field, 'related_name'))
-            #                                   )
-            #cls_attrs[field.field.name] = eval("{0}(source='{1}', "
-            #    "many=True, required=False, allow_null=True)".format(SubSerializer.__name__, M2MRelations(field, 'related_name'))
-            #)
+            cls_attrs["create"] = create
+            cls_attrs["update"] = update
+            
 
         except FieldDoesNotExist:
             pass

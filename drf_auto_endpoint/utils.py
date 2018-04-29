0c288cb78813f0d5847a8846a7b58de238abab87
Roger Agustin <roger@MacBook-MacBook-Pro-de-Roger.local>
2018-04-29 19:47:20 +0200

    Console logs

--- drf_auto_endpoint/utils.py
+++ drf_auto_endpoint/utils.py
@@ -71,11 +71,12 @@ def get_field_dict(field, serializer, translated_fields=None, fields_annotation=
         return {'key': name}
 
     read_only = name == '__str__'
-    print(":)")
-    print(read_only)
-#    if not read_only and field_instance.read_only:
-#        if not isinstance(field_instance, serializers.ManyRelatedField):
-#            read_only = True
+
+    if not read_only and field_instance.read_only:
+        print(":)")
+        print(read_only)
+        if not isinstance(field_instance, serializers.ManyRelatedField):
+            read_only = True
     print(":(")
     rv = {
         'key': name,

8aff24db5a2b04f072e70d4b0db6ab4b0a594214
Roger Agustin <roger@prenomics.com>
2018-07-08 10:42:03 +0200

    Post debugging

--- drf_auto_endpoint/factories.py
+++ drf_auto_endpoint/factories.py
@@ -242,12 +242,13 @@ def update(self, instance, validated_data):
                 if field_instance:
                     print("FIELD_INSTANCE")
                     print(field_instance)
+                    print(M2MRelations(field, 'related_field'))
                     # TODO Fer que es miri també si hi ha algun element diferent entre la nova info de f.name_data i la queryset anterior.
                     exec (
                         "field_instance.update({0}={1}, updated_at=datetime.now(), **rel_model_instance)".
                             format(
                             M2MRelations(field, 'related_field'),
-                            'instance'
+                            'instance.id'
 
                         )
                     )
